6d051eb0aee4597d3b5c170eea80eba48f51115d
Piotr <hunter976@gmail.com>
2025-05-14 16:44:34 +0200

    added clsx package and darkmode to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,59 @@
+import clsx from 'clsx';
 import Header from './comps/Header.jsx';
 import Extension from './comps/Extension.jsx';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode((prevV) => !prevV);
+  }
+
   return (
-    <div className="prime-container">
-      <Header />
+    <div
+      className={clsx(
+        'prime-container',
+        darkMode && 'prime-container-dark-mode'
+      )}
+    >
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
       <div className="upper-container">
-        <h2 className="text-preset-1">Extensions List</h2>
+        <h2 className={clsx('text-preset-1', darkMode && 'h2-dark-mode')}>
+          Extensions List
+        </h2>
         <div className="upper-btn-container">
-          <button className="upper-btn text-preset-4">All</button>
-          <button className="upper-btn text-preset-4">Active</button>
-          <button className="upper-btn text-preset-4">Inactive</button>
+          <button
+            className={clsx(
+              'upper-btn',
+              'text-preset-4',
+              darkMode && 'upper-btn-dark-mode'
+            )}
+          >
+            All
+          </button>
+          <button
+            className={clsx(
+              'upper-btn',
+              'text-preset-4',
+              darkMode && 'upper-btn-dark-mode'
+            )}
+          >
+            Active
+          </button>{' '}
+          <button
+            className={clsx(
+              'upper-btn',
+              'text-preset-4',
+              darkMode && 'upper-btn-dark-mode'
+            )}
+          >
+            Inactive
+          </button>
         </div>
       </div>
       <main>
-        <Extension />
-        <Extension />
-        <Extension />
-        <Extension />
-        <Extension />
-        <Extension />
-        <Extension />
+        <Extension darkMode={darkMode} />
       </main>
     </div>
   );
--- src/comps/Extension.jsx
+++ src/comps/Extension.jsx
@@ -1,21 +1,37 @@
 import img from '../assets/logo-speed-boost.svg';
+import clsx from 'clsx';
 
-function Extension() {
+function Extension({ darkMode }) {
   return (
-    <div className="item-box">
+    <div className={clsx('item-box', darkMode && 'item-box-dark-mode')}>
       <div className="item-info-box">
         <div className="info-box-img">
           <img src={img} alt="" />
         </div>
         <div className="info-box-info">
-          <h3 className="text-preset-2">DevLens</h3>
-          <p className="text-preset-5">
+          <h3 className={clsx('text-preset-2', darkMode && 'h3-dark-mode')}>
+            DevLens
+          </h3>
+          <p
+            className={clsx(
+              'text-preset-5',
+              'info-p',
+              darkMode && 'info-p-dark-mode'
+            )}
+          >
             Quickly inspect page layouts and visualize element boundaries.
           </p>
         </div>
       </div>
       <div className="item-btns-box">
-        <button className="remove-btn text-preset-6">Remove</button>
+        <button
+          className={clsx(
+            'remove-btn text-preset-6',
+            darkMode && 'remove-btn-dark-mode'
+          )}
+        >
+          Remove
+        </button>
         <div className="move-btn-active">
           <div className="move-btn-ball"></div>
         </div>
